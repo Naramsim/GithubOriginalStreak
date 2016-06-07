7a2969cb9b819f587633c837225b2d34ab15a31d
Alessandro Pezzè <igougi.ui@gmail.com>
2016-06-07 19:28:53 +0200

    minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,42 @@
-var gulp = require('gulp');
-var newer = require('gulp-newer');
-var chrome = './chrome/src/inject/*'
-var ff = './firefox/data/'
+const gulp = require('gulp')
+const combiner = require('stream-combiner2');
+const newer = require('gulp-newer')
+const babel = require('gulp-babel')
+const cssnano = require('gulp-cssnano')
+const uglify = require('gulp-uglify')
+const chrome = './chrome/src/inject/'
+const ff = './firefox/data/'
+const src = './src/*'
 
-gulp.task('sync', function() {
-	return gulp.src(chrome)
-		.pipe(newer(ff))
-		.pipe(gulp.dest(ff));
+gulp.task('syncCSS', function() {
+	var combined = combiner.obj([
+			gulp.src(src + '.css'),
+			newer(ff),
+			newer(chrome),
+			cssnano(),
+			gulp.dest(ff),
+			gulp.dest(chrome)
+		])
+	combined.on('error', console.error.bind(console));
+	return combined;
+});
+
+gulp.task('syncJS', function() {
+	var combined = combiner.obj([
+			gulp.src(src+ '.js'),
+			newer(ff),
+			newer(chrome),
+			babel({
+				presets: ['es2015']
+			}),
+			uglify(),
+			gulp.dest(ff),
+			gulp.dest(chrome)
+		])
+	combined.on('error', console.error.bind(console));
+	return combined;
 });
 
 gulp.task('watch', function() {
-	gulp.watch(chrome, ['sync']);
+	gulp.watch(src, ['syncCSS', 'syncJS'])
 })
