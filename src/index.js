c75916f00eba296b2594c2850875d0490a906654
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-06-15 15:28:52 -0700

    fix(regression): observe pjax-loader-bar only on nav click (#17)

--- src/index.js
+++ src/index.js
@@ -6,27 +6,38 @@ const changeColor = require('./change-color')
 inject()
 // invoke changeColor function
 changeColor()
+// attach click handler to navs
+attachNavClickHandler()
 
-/**
- * look for changes in the js-pjax-loader-bar
- * and invoke inject function
- * to inject github streak container
- */
-const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.oldValue && mutation.oldValue.indexOf("is-loading") > -1 ) {
-            // invoke inject function
-            inject()
-            // invoke changeColor function
-            changeColor()
-        }
+function navClickHandler() {
+    /**
+     * look for changes in the js-pjax-loader-bar
+     * and invoke inject function
+     * to inject github streak container
+     */
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.oldValue && mutation.oldValue.indexOf("is-loading") > -1 ) {
+                // stop observing
+                observer.disconnect()
+                // invoke inject function
+                inject()
+                // invoke changeColor function
+                changeColor()
+                // attach clickHandler to nav as it has been detached
+                attachNavClickHandler()
+            }
+        })
     })
-})
+    /**
+     * observe pjaxLoaderBar for changes in attributes
+     */
+    const pjaxLoaderBar = document.getElementById("js-pjax-loader-bar")
+    const config = { attributes: true, attributeOldValue: true }
+    observer.observe(pjaxLoaderBar, config)
+}
 
-/**
- * observe pjaxLoaderBar for changes in attributes
- */
-const pjaxLoaderBar = document.getElementById("js-pjax-loader-bar")
-const config = { attributes: true, attributeOldValue: true }
-observer.observe(pjaxLoaderBar, config)
+function attachNavClickHandler() {
+    document.querySelector("nav").addEventListener('click', navClickHandler, false)
+}
 
