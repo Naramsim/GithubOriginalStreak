e5bfde3052091bbdd11a629b5bd7f57be827fd47
Alessandro Pezzè <igougi.ui@gmail.com>
2016-06-17 23:11:35 +0200

    Multiple contributions palettes

--- src/change-color.js
+++ src/change-color.js
@@ -5,22 +5,31 @@ function updateStyle(selector, property, value) {
     });
 }
 
-function changeColor() {
-    const color = localStorage.getItem('blueSky');
-    const colorsHash = {
-        'eeeeee': 'eeeeee',
-        'd6e685': '9DC1F6',
-        '8cc665': '629DF3',
-        '44a340': '2B7BF2',
-        '1e6823': '0363F0'
+function changeContributionsColor() {
+    const color = localStorage.getItem('color-scheme');
+    const colorScheme = {
+        'blue-sky': {
+            'eeeeee': 'eeeeee',
+            'd6e685': '9DC1F6',
+            '8cc665': '629DF3',
+            '44a340': '2B7BF2',
+            '1e6823': '0363F0'
+        },
+        'halloween': {
+            'eeeeee': 'eeeeee',
+            'd6e685': 'FFEE58',
+            '8cc665': 'FFCA28',
+            '44a340': 'EF6C00',
+            '1e6823': '000000'
+        }
     };
 
-    if (color) {
-        Object.keys(colorsHash).forEach(key => {
-            updateStyle(`rect[fill='#${key}']`, 'fill', `#${colorsHash[key]}`);
-            updateStyle(`li[style='background-color: #${key}']`, 'backgroundColor', `#${colorsHash[key]}`);
+    if (color && (color in colorScheme)) {
+        Object.keys(colorScheme[color]).forEach(key => {
+            updateStyle(`rect[fill='#${key}']`, 'fill', `#${colorScheme[color][key]}`);
+            updateStyle(`li[style='background-color: #${key}']`, 'backgroundColor', `#${colorScheme[color][key]}`);
         });
     }
 }
 
-module.exports = changeColor;
+module.exports = changeContributionsColor;
